49fdccf4a6227fdf3c2319c88cb73ebb70f717b4
Ural-k <mission171011@gmail.com>
2026-04-15 12:00:56 +0900

    Completed:CricleAOE

--- RBST/Assets/Userfile/Ural/Script/CricleAOEControll.cs
+++ RBST/Assets/Userfile/Ural/Script/CricleAOEControll.cs
@@ -4,11 +4,13 @@ public class CricleAOEControll : MonoBehaviour
 {
     [SerializeField] private GameObject entity_; //AOEの実体
     [SerializeField] private GameObject entry_;  //AOEの予兆
-    [SerializeField] private float timer_;       //経過時間
     [SerializeField] private float entryTime_;   //予兆時間
     [SerializeField] private float entityTime_;  //実体時間
-    private bool entryActiveFlag_;                    //起動フラグ
-    private bool entityActiveFlag_;                   //実体起動フラグ
+    private float timer_;                       //経過時間
+    private bool entryActiveFlag_;               //起動フラグ
+    private bool entityActiveFlag_;              //実体起動フラグ
+    
+
     void Start()
     {
         entity_.SetActive(false);
@@ -21,19 +23,29 @@ public class CricleAOEControll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //デバッグ用
         if (Input.GetMouseButtonDown(0)) 
         {
             isActive();
         }
+
         AOEEntry();
         AOEEntity();
     }
 
+    /// <summary>
+    /// 予兆を生成するフラグをオンにする
+    /// </summary>
     public void isActive()
     {
+        if (entityActiveFlag_) { return; }
         if (entryActiveFlag_) { return; }
-        else{ entryActiveFlag_ = true; }
+        else{ entryActiveFlag_ = true; Debug.Log("s"); }
     }
+
+    /// <summary>
+    /// 予兆を生成一定時間後に実体に移行
+    /// </summary>
 
     public void AOEEntry()
     {
@@ -43,13 +55,16 @@ public class CricleAOEControll : MonoBehaviour
         if (timer_ < 0)
         {
             entry_.SetActive(false);
-            entityActiveFlag_ = true;
             entryActiveFlag_ = false;
+            entityActiveFlag_ = true;
             timer_ = entityTime_;
         }
 
     }
 
+    /// <summary>
+    /// 実体を生成一定時間後に使った変数をリセット
+    /// </summary>
     public void AOEEntity()
     {
         if (!entityActiveFlag_) { return; }
@@ -58,7 +73,6 @@ public class CricleAOEControll : MonoBehaviour
         if (timer_ < 0)
         {
             entity_.SetActive(false);
-            entryActiveFlag_ = false;
             entityActiveFlag_ = false;
             timer_ = entityTime_;
         }
