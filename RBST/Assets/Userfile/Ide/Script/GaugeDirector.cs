ee526cf2d7ad1ed5c0c3108f10eb2b0d8d5e2f0a
nkc-ide <nkc.ide.satoshi@gmail.com>
2026-04-21 15:39:08 +0900

    Created:Gauge/Update:Gauge/ Add:Script

--- RBST/Assets/Userfile/Ide/Script/GaugeDirector.cs
+++ RBST/Assets/Userfile/Ide/Script/GaugeDirector.cs
@@ -4,22 +4,28 @@ using UnityEngine.UI;
 public class GaugeDirector : MonoBehaviour
 {
     //ゲージUI
-    public Image gauge_;
+    [SerializeField]
+    private Image gaugeImage_;
 
     //仮のUI
-    public Image kari_;
+    [SerializeField]
+    private Image demoImage_;
 
     //半透明UI
-    public Image kuro_;
+    [SerializeField]
+    private Image backgroundImage_;
 
-    //数字Text
-    public Text suuji_;
+    //ゲージUIと一緒に表示するText
+    [SerializeField]
+    private Text countText_;
 
     //最大時間(秒)
-    public float maxTime_;
+    [SerializeField]
+    private float maxTime_;
 
     //自動開始をする
-    public bool autoStart = true;
+    [SerializeField]
+    private bool isAutoStart_ = true;
 
     //残り時間
     private float currentTime_;
@@ -27,82 +33,75 @@ public class GaugeDirector : MonoBehaviour
     //動作中のフラグ
     private bool isRunning_;
 
-    void Start()
+    private void Start()
     {
-        //ゲージと半透明と数字が非表示になる
-        gauge_.gameObject.SetActive(false);
-        kuro_.gameObject.SetActive(false);
-        suuji_.gameObject.SetActive(false);
+        //ゲージUIと半透明UIと数字Textは非表示にする
+        gaugeImage_.gameObject.SetActive(false);
+        backgroundImage_.gameObject.SetActive(false);
+        countText_.gameObject.SetActive(false);
 
-        //仮のImageを表示させる
-        kari_.gameObject.SetActive(true);
+        //仮のUIだけ表示にする
+        demoImage_.gameObject.SetActive(true);
     }
 
-    void Update()
+    private void Update()
     {
-        //特定のキーを押したら
-        if(Input.GetKeyDown(KeyCode.Q))
+        //特定のキー入力でゲージ開始
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            //スタート関数
             StartGauge();
         }
 
-        if (!isRunning_)
+        // ゲージが動作していない場合は更新処理を行わない
+        if (isRunning_ == false)
         {
             return;
         }
 
-        //残り時間から減らす
+        //実時間ベースで残り時間を減らす
         currentTime_ -= Time.deltaTime;
 
-        //0以下にならない
+        // 残り時間が0を下回ったらカウントダウン終了処理を行う
         if (currentTime_ < 0f)
         {
-            //0になったら
             currentTime_ = 0f;
-
-            //停止する
             isRunning_ = false;
-
-            //リセット関数
             ResetGauge();
         }
 
         //ゲージのUIが減るのと同時に数字のTextも減っていく
-        suuji_.text = Mathf.CeilToInt(currentTime_).ToString();
+        countText_.text = Mathf.CeilToInt(currentTime_).ToString();
 
         //0～1の範囲
-        gauge_.fillAmount = currentTime_ / maxTime_;
+        gaugeImage_.fillAmount = currentTime_ / maxTime_;
     }
 
-    //ゲージを開始する
+    // ゲージを開始する
     public void StartGauge()
     {
-        //残り時間をMaxに戻す
+        //ゲージ開始時に残り時間を最大値に戻し、カウントダウンを開始する
         currentTime_ = maxTime_;
-
-        //動作を開始する
         isRunning_ = true;
 
-        //ゲージと半透明と数字が表示させる
-        gauge_.gameObject.SetActive(true);
-        kuro_.gameObject.SetActive(true);
-        suuji_.gameObject.SetActive(true);
+        //ゲージUIと半透明UIと数字Textと仮のUIを表示させる
+        gaugeImage_.gameObject.SetActive(true);
+        backgroundImage_.gameObject.SetActive(true);
+        countText_.gameObject.SetActive(true);
+        demoImage_.gameObject.SetActive(true);
     }
 
-    //ゲージをリセットする
+    // ゲージをリセットする
     public void ResetGauge()
     {
         //Maxに戻す
-        gauge_.fillAmount = 1f;
+        gaugeImage_.fillAmount = 1f;
 
         //停止する
         isRunning_ = false;
 
-        //半透明と数字が非表示になる
-        kuro_.gameObject.SetActive(false);
-        suuji_.gameObject.SetActive(false);
+        backgroundImage_.gameObject.SetActive(false);
+        countText_.gameObject.SetActive(false);
 
-        kari_.gameObject.SetActive(true);
+        demoImage_.gameObject.SetActive(true);
     }
 }
