2d623d801015a10302446a8f2ee7599dd05c8676
nkc-ide <nkc.ide.satoshi@gmail.com>
2026-04-22 11:45:09 +0900

    Created:Gauge/Update:Gauge/ Add:Script

--- RBST/Assets/Userfile/Ide/Script/GaugeDirector.cs
+++ RBST/Assets/Userfile/Ide/Script/GaugeDirector.cs
@@ -3,35 +3,15 @@ using UnityEngine.UI;
 
 public class GaugeDirector : MonoBehaviour
 {
-    //ゲージUI
-    [SerializeField]
-    private Image gaugeImage_;
-
-    //仮のUI
-    [SerializeField]
-    private Image demoImage_;
-
-    //半透明UI
-    [SerializeField]
-    private Image backgroundImage_;
-
-    //ゲージUIと一緒に表示するText
-    [SerializeField]
-    private Text countText_;
-
-    //最大時間(秒)
-    [SerializeField]
-    private float maxTime_;
-
-    //自動開始をする
-    [SerializeField]
-    private bool isAutoStart_ = true;
-
-    //残り時間
-    private float currentTime_;
-
-    //動作中のフラグ
-    private bool isRunning_;
+    
+    [SerializeField] private Image gaugeImage_;          //ゲージUI
+    [SerializeField] private Image demoImage_;           //仮のUI
+    [SerializeField] private Image backgroundImage_;     //半透明UI
+    [SerializeField] private Text countText_;            //ゲージUIと一緒に表示するText
+    [SerializeField] private float maxTime_;             //最大時間(秒)
+    [SerializeField] private bool isAutoStart_ = true;   //自動開始をする
+    private float currentTime_;                          //残り時間
+    private bool isRunning_;                             //動作中のフラグ
 
     private void Start()
     {
@@ -47,21 +27,15 @@ public class GaugeDirector : MonoBehaviour
     private void Update()
     {
         //特定のキー入力でゲージ開始
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            StartGauge();
-        }
+        if (Input.GetKeyDown(KeyCode.Q)) { StartGauge(); }
 
-        // ゲージが動作していない場合は更新処理を行わない
-        if (isRunning_ == false)
-        {
-            return;
-        }
+        //ゲージが動作していない場合は更新処理を行わない
+        if (isRunning_ == false) { return; }
 
         //実時間ベースで残り時間を減らす
         currentTime_ -= Time.deltaTime;
 
-        // 残り時間が0を下回ったらカウントダウン終了処理を行う
+        //残り時間が0を下回ったらカウントダウン終了処理を行う
         if (currentTime_ < 0f)
         {
             currentTime_ = 0f;
@@ -76,7 +50,7 @@ public class GaugeDirector : MonoBehaviour
         gaugeImage_.fillAmount = currentTime_ / maxTime_;
     }
 
-    // ゲージを開始する
+    //ゲージを開始する
     public void StartGauge()
     {
         //ゲージ開始時に残り時間を最大値に戻し、カウントダウンを開始する
@@ -90,7 +64,7 @@ public class GaugeDirector : MonoBehaviour
         demoImage_.gameObject.SetActive(true);
     }
 
-    // ゲージをリセットする
+    //ゲージをリセットする
     public void ResetGauge()
     {
         //Maxに戻す
