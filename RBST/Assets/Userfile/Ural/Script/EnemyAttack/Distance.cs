1daadf757627accd51e925b3cc6eb2ea51c115cb
Ural-k <mission171011@gmail.com>
2026-05-07 14:20:08 +0900

    add::Distance

---
+++ RBST/Assets/Userfile/Ural/Script/EnemyAttack/Distance.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Distance : AOEControll
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override void isActive()
+    {
+        base.isActive();
+    }
+
+    public override void Entry()
+    {
+        base.Entry();
+    }
+
+    public override void Entity()
+    {
+        base.Entity();
+    }
+}
