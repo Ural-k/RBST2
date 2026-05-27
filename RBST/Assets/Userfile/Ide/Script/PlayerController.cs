35f669f3f50af6289d7578d77c804787af4e0359
nkc-ide <nkc.ide.satoshi@gmail.com>
2026-05-27 14:56:08 +0900

    Update:HomeScene&Script/Add:HomeScene&Script

--- RBST/Assets/Userfile/Ide/Script/PlayerController.cs
+++ RBST/Assets/Userfile/Ide/Script/PlayerController.cs
@@ -2,14 +2,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] public GameObject cube;
-    [SerializeField] public float speed;
-    private Rigidbody2D playerRigidbody;
+    [SerializeField] public Transform cube_;
+    [SerializeField] public float speed_;
+    private Rigidbody2D playerRigidbody_;
+
+    private void Start()
+    {
+        playerRigidbody_ = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector2 move = new Vector2(horizontal, vertical);
+
+        cube_.Translate(move * speed_ * Time.deltaTime);
     }
 }
