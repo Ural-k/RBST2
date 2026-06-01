6f28181e1cfd65340e74b9a34b955657e9bf551c
MSI\user <taiseihoukan9@gmail.com>
2026-06-01 15:00:29 +0900

    uenagiharai

--- RBST/Assets/Userfile/Ural/Script/Enemy/EnemyControl.cs
+++ RBST/Assets/Userfile/Ural/Script/Enemy/EnemyControl.cs
@@ -48,12 +48,24 @@ public class EnemyControl : MonoBehaviour
             //攻撃のステータスを設定（すくたぶがいいなぁ（ちらちら）
             tfStruct.pos = new Vector2(0,0);    //攻撃を出す座標
             tfStruct.innerRadius = 0f;          //内側の円の半径（ドーナツ使用時以外０）
-            tfStruct.scale = 0f;                //攻撃のサイズ
+            tfStruct.scale = 2f;                //攻撃のサイズ
             //↓ここだけ必須
             colect_ = AOECollect.Circle;        //攻撃の種類を設定
             //攻撃の表示と再生
             var GetAttack = pool_.GetObject(colect_);
             GetAttack.IsActive(tfStruct);
+
+            yield return new WaitForSeconds(waitTime);
+
+            colect_ = AOECollect.Box;
+            tfStruct.pos = new Vector2(0, 30);
+            tfStruct.scale = 55;
+
+            var uenagiharai = pool_.GetObject(colect_);
+            uenagiharai.IsActive(tfStruct);
+
+            colect_ = AOECollect.Box;
+            
 
             //↑　好きに改造してね♡
             yield return new WaitForSeconds(waitTime);
