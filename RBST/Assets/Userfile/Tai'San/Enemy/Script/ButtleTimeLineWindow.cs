650bff4704e47787b316da6a2eef20ac98e820f4
Tai-desktop <taiseihoukan6@gmail.com>
2026-06-02 00:23:42 +0900

    ADD:EditorWindow

---
+++ RBST/Assets/Userfile/Tai'San/Enemy/Script/ButtleTimeLineWindow.cs
@@ -0,0 +1,38 @@
+using UnityEditor;
+using UnityEngine;
+
+public class ButtleTimeLineWindow : EditorWindow 
+{
+    //[MenuItem("Window/ButtleTimeLine")]
+    //public static void CreateWindow()
+    //{
+    //    EditorWindow.GetWindow<ButtleTimeLineWindow>(title: "BattleTimeLine");
+    //}
+
+    //private void CreateGUI()
+    //{
+    //    Rect canvasRect = new Rect(0, 20, position.width, position.height - 220);
+    //    DrawGrid(20);
+    //}
+
+    //private void DrawGrid(float gridSize)
+    //{
+    //    Handles.color = new Color(0.3f, 0.3f, 0.3f);
+
+    //    // 縦線
+    //    for (float x = 0; x < position.width; x += gridSize)
+    //    {
+    //        Handles.DrawLine(
+    //            new Vector3(x, 0),
+    //            new Vector3(x, position.height));
+    //    }
+
+    //    // 横線
+    //    for (float y = 0; y < position.height; y += gridSize)
+    //    {
+    //        Handles.DrawLine(
+    //            new Vector3(0, y),
+    //            new Vector3(position.width, y));
+    //    }
+    //}
+}
