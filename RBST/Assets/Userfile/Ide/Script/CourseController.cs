22c8b108248b925a6e98747ca9a50bc611a3f303
nkc-ide <nkc.ide.satoshi@gmail.com>
2026-06-10 11:39:52 +0900

    Add:CourseController

---
+++ RBST/Assets/Userfile/Ide/Script/CourseController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CourseController : MonoBehaviour
+{
+    private Button[] buttons_;
+    private int currentIndex = 0;
+    private bool isSwapped = false;
+
+    private void Start()
+    {
+        
+    }
+}
