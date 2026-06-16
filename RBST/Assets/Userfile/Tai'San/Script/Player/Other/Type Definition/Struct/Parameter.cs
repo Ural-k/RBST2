85b57840467d6bdeb47448bae2be97042fa9a8cd
MSI\user <taiseihoukan9@gmail.com>
2026-06-16 15:19:29 +0900

    PlayerSkill

--- RBST/Assets/Userfile/Tai'San/Script/Player/Other/Type Definition/Struct/Parameter.cs
+++ RBST/Assets/Userfile/Tai'San/Script/Player/Other/Type Definition/Struct/Parameter.cs
@@ -1,13 +1,20 @@
+using UnityEngine;
+
 [System.Serializable]//プレイヤーの状態数値
 public struct Parameter
 {
     public int jobNumber_;          //職業
     public string playerName_;      //プレイヤーの名前x
-    public float maxHp_;            //最大HPx
-    public float hp_;               //現在のHPx
+    public int maxHp_;              //最大HPx
+    private int hp_;                 //現在のHPx
     public float speed_;            //移動速度o
     public float attack_;           //攻撃力x
     public float defense_;          //防御力x
     public float critical_;         //クリティカル率x
     public uint lv_;                //現在のレベルx
+
+    /// <summary>
+    /// 0～maxHp
+    /// </summary>
+    public int HP { get { return hp_; } set { hp_ = Mathf.Clamp(value, 0, maxHp_); } }
 }
