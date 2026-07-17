6720d40802207bd49959c8ac41d146d2d39825fe
MSI\user <taiseihoukan9@gmail.com>
2026-07-17 13:57:19 +0900

    Player2

---
+++ RBST/Assets/Userfile/Tai'San/Script/Player2/TestSkill.cs
@@ -0,0 +1,91 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// プレイヤーのスキル　攻撃は基本的に円形を指す
+/// </summary>
+public class TestSkill : MonoBehaviour
+{
+    public static TestSkill Instance;
+
+    /*
+     *  範囲取得
+     */
+    /// <summary>
+    /// 一番近い敵中心の範囲に当たった敵の取得
+    /// </summary>
+    /// <param name="from">基準座標</param>
+    /// <param name="radius">半径</param>
+    public Transform[] GetHitEnemyNear(Vector2 from, float radius)
+    {
+        if (EnemyManager.GetAllEnemyListCount() != 0)
+        {
+            List<Transform> target = new List<Transform>();
+            for (int i = 0; i < EnemyManager.GetAllEnemyListCount(); ++i) target.Add(EnemyManager.GetEnemy(i).gameObject.transform);
+            var near = target.OrderBy(n => Vector2.Distance(from, n.position)).First();
+            return target.Where(n => Vector2.Distance(near.position, n.position) <= radius + n.GetComponent<TargetCircle>().GetRadius).ToArray();
+        }
+        else return null;
+    }
+    /// <summary>
+    /// 範囲に当たった敵の取得
+    /// </summary>
+    /// <param name="center">中心</param>
+    /// <param name="radius">半径</param>
+    public Transform[] GetHitEnemy(Vector2 center, float radius)
+    {
+        if (EnemyManager.GetAllEnemyListCount() != 0)
+        {
+            List<Transform> target = new List<Transform>();
+            for (int i = 0; i < EnemyManager.GetAllEnemyListCount(); ++i) target.Add(EnemyManager.GetEnemy(i).gameObject.transform);
+            return target.Where(n => Vector2.Distance(center, n.position) <= radius + n.GetComponent<TargetCircle>().GetRadius).ToArray();
+        }
+        else return null;
+    }
+    /// <summary>
+    /// 一番近い味方中心の範囲に当たった味方の取得
+    /// </summary>
+    /// <param name="from">基準座標</param>
+    /// <param name="radius">半径</param>
+    public Transform[] GetHitPlayerNear(Vector2 from, float radius)
+    {
+        if (PlayerManager.GetAllPlayerListCount() != 0)
+        {
+            List<Transform> target = new List<Transform>();
+            for (int i = 0; i < PlayerManager.GetAllPlayerListCount(); ++i) target.Add(PlayerManager.GetPlayer(i).gameObject.transform);
+            var near = target.OrderBy(n => Vector2.Distance(from, n.position)).First();
+            return target.Where(n => Vector2.Distance(near.position, n.position) <= radius + n.GetComponent<TargetCircle>().GetRadius).ToArray();
+        }
+        else return null;
+    }
+    /// <summary>
+    /// 範囲に当たった味方の取得
+    /// </summary>
+    /// <param name="center">中心</param>
+    /// <param name="radius">半径</param>
+    public Transform[] GetHitPlayer(Vector2 center, float radius)
+    {
+        if (PlayerManager.GetAllPlayerListCount() != 0)
+        {
+            List<Transform> target = new List<Transform>();
+            for (int i = 0; i < PlayerManager.GetAllPlayerListCount(); ++i) target.Add(PlayerManager.GetPlayer(i).gameObject.transform);
+            return target.Where(n => Vector2.Distance(center, n.position) <= radius + n.GetComponent<TargetCircle>().GetRadius).ToArray();
+        }
+        else return null;
+
+    }
+
+
+}
+
+interface Defence
+{
+    /// <summary>
+    /// ダメージを与える
+    /// </summary>
+    /// <param name="point">ダメージ量</param>
+    void DamageAble(int point);
+
+
+}
