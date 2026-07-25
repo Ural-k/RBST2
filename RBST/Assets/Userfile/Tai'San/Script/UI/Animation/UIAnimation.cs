42ef56fb7a9be2cc0b4294251faf57c1f4d689a8
Tai-desktop <taiseihoukan6@gmail.com>
2026-07-25 19:26:25 +0900

    UI

--- RBST/Assets/Userfile/Tai'San/Script/UI/Animation/UIAnimation.cs
+++ RBST/Assets/Userfile/Tai'San/Script/UI/Animation/UIAnimation.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIAnimation : MonoBehaviour
 {
     [SerializeField] private float loopTime_;
-    [SerializeField] private float startScale_;
     [SerializeField] private float changeScale_;
     [SerializeField] private AnimationType animationType_;
+    [SerializeField] private Color endColor_;
+    private float startScale_;
+    private Text text_;
+    private Color startColor_;
 
     private enum AnimationType
     {
         None,
         ScalePingPong,
+        TextColorPingPong,
+        CenterLineAndText
+    }
+    private void Start()
+    {
+        switch (animationType_)
+        {
+            case AnimationType.ScalePingPong:
+                transform.localScale = Vector2.one * (Mathf.PingPong(Time.time / loopTime_, changeScale_) + startScale_);
+                break;
+            case AnimationType.TextColorPingPong:
+                text_.color = new Color();
+                break;
+            default: break;
+        }
     }
 
     private void Update()
@@ -20,7 +39,15 @@ public class UIAnimation : MonoBehaviour
             case AnimationType.ScalePingPong:
                 transform.localScale = Vector2.one * (Mathf.PingPong(Time.time / loopTime_, changeScale_) + startScale_);
                 break;
+            case AnimationType.TextColorPingPong:
+                text_.color = new Color();
+                break;
             default: break;
         }
+    }
+
+    public void OnCenterLineText()
+    {
+
     }
 }
