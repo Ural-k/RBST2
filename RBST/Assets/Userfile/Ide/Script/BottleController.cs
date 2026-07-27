de3348a073c88e35f5621cbb72ce782eef4a0e5b
nkc-ide <nkc.ide.satoshi@gmail.com>
2026-07-28 05:08:28 +0900

    Update:BataUI/Add:BataUI

--- RBST/Assets/Userfile/Ide/Script/BottleController.cs
+++ RBST/Assets/Userfile/Ide/Script/BottleController.cs
@@ -1,24 +1,44 @@
 using UnityEngine;
 
-
 public class BottleController : MonoBehaviour
 {
+    //ボトルが飛ぶ速度
+    public float speed;
 
+    //ボトルの回転速度
+    public float rotateSpeed;
 
-    public float rotateSpeed = 720f;
+    //ボトルが存在できる時間
+    //時間が経過すると自動的に削除される
+    public float lifeTime;
 
+    //ボトルが進む方向
+    //初期値は右方向
+    private Vector2 direction = Vector2.right;
 
-
-    void Update()
+    void Start()
     {
-
-        transform.Rotate(
-        0,
-        0,
-        rotateSpeed *
-        Time.deltaTime);
-
+        //lifeTime秒後にボトルを削除する
+        Destroy(gameObject, lifeTime);
     }
 
+    //プレイヤー側から投げる方向を受け取る処理
+    //PlayerControllerから呼び出される
+    public void SetDirection(Vector2 dir)
+    {
+        //受け取った方向を移動方向として保存
+        direction = dir;
+    }
 
+    //毎フレーム呼ばれる処理
+    void Update()
+    {
+        //設定された方向へボトルを移動させる
+        //Time.deltaTimeを使うことでフレームレートに左右されない速度になる
+        transform.position += (Vector3)(direction * speed * Time.deltaTime);
+
+        //ボトルを回転させる
+        //投げられている感じを出すための演出
+        transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+    }
 }
--- RBST/Assets/Userfile/Ide/Script/PlayerController.cs
+++ RBST/Assets/Userfile/Ide/Script/PlayerController.cs
@@ -1,79 +1,117 @@
 using UnityEngine;
 
-
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
+    //プレイヤーの移動速度
+    public float speed;
 
-    Rigidbody2D rb;
+    //プレイヤーアニメーションを制御するスクリプト
+    public PlayerAnimation animationController;
 
-    Animator animator;
-
-    Vector2 movement;
-
+    //ボトルを生成する位置
     public Transform throwPoint;
 
+    //投げるボトルのPrefab
     public GameObject bottlePrefab;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
+    //プレイヤーが向いている方向
+    private int facingDirection = -1;
 
-        animator =
-        GetComponent<Animator>();
-    }
+    //攻撃中かどうかを管理するフラグ
+    //攻撃中は移動や追加攻撃をできないようにする
+    private bool attacking;
 
     void Update()
     {
-        // W A S D
-        movement.x =
-        Input.GetAxisRaw("Horizontal");
-
-        movement.y =
-        Input.GetAxisRaw("Vertical");
-
-        if (movement != Vector2.zero)
+        //攻撃中ではない場合のみ操作を受け付ける
+        if (!attacking)
         {
-            animator.SetBool(
-            "isWalking",
-            true);
+            //プレイヤー移動処理
+            Move();
+
+            //Enterキー入力でボトルを投げる
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                ThrowBottle();
+            }
+        }
+    }
+
+    //プレイヤーの移動処理
+    void Move()
+    {
+        //キーボード入力を取得
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        //入力方向をVector2にまとめる
+        Vector2 move = new Vector2(x, y);
+
+        //入力がある場合
+        if (move != Vector2.zero)
+        {
+            //移動方向を正規化して速度を一定にする
+            //normalizedを使うことで斜め移動時の速度上昇を防ぐ
+            transform.position += (Vector3)move.normalized * speed * Time.deltaTime;
+
+            //歩行アニメーション開始
+            animationController.SetWalking(true);
+
+            //左方向へ移動した場合
+            if (x < 0)
+            {
+                //キャラクターを左向きに反転
+                transform.localScale = new Vector3(0.15f, 0.15f, 0.15f);
+
+                //投げる方向を左に設定
+                facingDirection = -1;
+            }
+
+            //右方向へ移動した場合
+            else if (x > 0)
+            {
+                //キャラクターを右向きに反転
+                transform.localScale = new Vector3(-0.15f, 0.15f, 0.15f);
+
+                //投げる方向を右に設定
+                facingDirection = 1;
+            }
         }
         else
         {
-            animator.SetBool(
-            "isWalking",
-            false);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            ThrowBottle();
+            //入力がない場合は待機アニメーションへ
+            animationController.SetWalking(false);
         }
     }
 
-    void FixedUpdate()
-    {
-        rb.MovePosition(
-        rb.position +
-        movement.normalized *
-        speed *
-        Time.fixedDeltaTime);
-    }
-
+    //ボトルを投げる処理
     void ThrowBottle()
     {
-        animator.SetTrigger("Throw");
+        //投げるアニメーションを再生
+        animationController.StartThrow();
 
-        GameObject bottle =
-        Instantiate(
-        bottlePrefab,
-        throwPoint.position,
-        Quaternion.identity);
+        //throwPointの位置にボトルを生成
+        GameObject bottle = Instantiate(bottlePrefab,throwPoint.position,Quaternion.identity);
 
-        Rigidbody2D bottleRb =
-        bottle.GetComponent<Rigidbody2D>();
+        //生成したボトルの制御スクリプトを取得
+        BottleController bottleScript = bottle.GetComponent<BottleController>();
 
-        bottleRb.linearVelocity =
-        transform.right * 8f;
+        //ボトルを飛ばす方向
+        Vector2 throwDirection;
+
+        //プレイヤーの向いている方向によって投げる向きを決定
+        if (facingDirection == 1)
+        {
+            //右向きの場合
+            throwDirection = Vector2.right;
+        }
+        else
+        {
+            //左向きの場合
+            throwDirection = Vector2.left;
+        }
+
+        //ボトル側のスクリプトへ投げる方向を渡す
+        bottleScript.SetDirection(throwDirection);
     }
 }
